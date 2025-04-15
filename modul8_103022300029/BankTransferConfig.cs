dcc9ad78e2f09201c23e535e05bc7f4523c47064
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:14:01 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_103022300029/BankTransferConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300029
+{
+    class BankTransferConfig
+    {
+        public const string ConfigPath = @"bank_transfer_config.json";
+
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public List<string> methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+
+        public class Transfer()
+        {
+
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+
+        }
+
+        public class Confirmation()
+        {
+
+            public string en { get; set; }
+            public string id { get; set; }
+
+
+
+        }
+
+        public BankTransferConfig LoadConfig()
+        {
+            string json = File.ReadAllText(ConfigPath);
+            var data = JsonSerializer.Deserialize<BankTransferConfig>(json);
+            return data;
+        }
+    }
+
+}
--- modul8_103022300029/Program.cs
+++ modul8_103022300029/Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8_103022300029;
+
+class Program
+{
+    static void Main()
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        BankTransferConfig data = config.LoadConfig();
+        int totalAmount;
+
+        if (data.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+            int uang = Convert.ToInt32(Console.ReadLine());
+
+
+            if (uang <= data.transfer.threshold)
+            {
+
+                totalAmount = uang + data.transfer.low_fee;
+
+            }
+            else
+            {
+
+                totalAmount = uang + data.transfer.high_fee;
+            }
+            Console.WriteLine("\nSelect transfer method: ");
+            for (int i = 0; i < data.methods.Count; i++)
+            {
+                Console.WriteLine((i + 1) + " " + data.methods[i]);
+            }
+            int metode = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("\nPlease type " + data.confirmation.en + " to confirm the transaction");
+
+            string konfirmasi = Convert.ToString(Console.ReadLine());
+            if (konfirmasi != data.confirmation.en)
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+            else
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+            int uang = Convert.ToInt32(Console.ReadLine());
+
+
+            if (uang <= data.transfer.threshold)
+            {
+
+                totalAmount = uang + data.transfer.low_fee;
+
+            }
+            else
+            {
+
+                totalAmount = uang + data.transfer.high_fee;
+            }
+            Console.WriteLine("\nPilih metode transfer: ");
+            for (int i = 0; i < data.methods.Count; i++)
+            {
+                Console.WriteLine((i + 1) + " " + data.methods[i]);
+            }
+            int metode = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("\nKetik " + data.confirmation.id + "  untuk mengkonfirmasi transaksi:");
+
+            string konfirmasi = Convert.ToString(Console.ReadLine());
+            if (konfirmasi != data.confirmation.en)
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
+
+        }
+    }
+
+    
+}
